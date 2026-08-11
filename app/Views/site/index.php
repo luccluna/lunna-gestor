aefd8a97436c87b513346bb111b6c4c013e7729b
Lucca <lucca@local.test>
2026-08-11 13:56:25 -0300

    Refina responsividade geral do admin

--- app/Views/site/index.php
+++ app/Views/site/index.php
@@ -78,7 +78,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= base_url('assets/css/style.css?v=20260811-5') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/style.css?v=20260811-6') ?>" rel="stylesheet">
 </head>
 
 <body class="site-page">
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- CSS próprio -->
-    <link href="<?= base_url('assets/css/style.css?v=20260811-5') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/style.css?v=20260811-6') ?>" rel="stylesheet">
 </head>
 
 <body>
