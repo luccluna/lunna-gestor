867a928e225c9c7becfd7207bcbe18adb745d89e
Lucca <lucca@local.test>
2026-08-11 15:33:15 -0300

    Corrige limite lateral no mobile

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="<?= base_url('assets/img/lunna/favicon.png') ?>">
-    <link href="<?= base_url('assets/css/style.css?v=20260811-7') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/style.css?v=20260811-8') ?>" rel="stylesheet">
 </head>
 
 <body class="login-page" style="--login-hero-image: url('<?= $hero ?>');">
