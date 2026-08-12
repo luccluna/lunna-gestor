6df0ddf439d9e58e205f1a621125ec9563994f64
Lucca <lucca@local.test>
2026-08-11 21:28:06 -0300

    Corrige alinhamento do campo de arquivo

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- CSS próprio -->
-    <link href="<?= base_url('assets/css/style.css?v=20260811-8') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/style.css?v=20260811-9') ?>" rel="stylesheet">
 </head>
 
 <body>
